023ff3c7d16d9890b09881f6cfd10842d01f4f7a
Amadou <amadoucoulibaly618@gmail.com>
2023-10-14 12:14:46 +0200

    Swagger configuration

---
+++ src/WebApi/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,26 @@
+namespace WebApi.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static IHostBuilder AddConfiguration(this IHostBuilder host)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        host.ConfigureAppConfiguration((builderCtx, configurationBuilder) =>
+        {
+            configurationBuilder.AddJsonFile(
+                path: "appsettings.json",
+                optional: false,
+                reloadOnChange: true);
+
+            configurationBuilder.AddJsonFile(
+                path: $"appsettings.{environment}.json",
+                optional: true,
+                reloadOnChange: true);
+
+            configurationBuilder.AddEnvironmentVariables();
+        });
+
+        return host;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.AddConfiguration();
+
+builder.Services.AddSingleton<ILogger>(sp => sp.GetRequiredService<ILogger<Program>>());
+
+builder.Services
+    .AddSwaggerGen()
+    .AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+app.MapHealthChecks("/health");
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapWhen(
+    ctx => ctx.Request.Path == "/",
+    appBuilder => appBuilder.Run(async ctx => await Task.Run(() => ctx.Response.Redirect("/swagger"))));
 
 app.Run();
+
